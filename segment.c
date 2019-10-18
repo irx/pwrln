358c0a277179bb86e6f48861981bebc369b4e911
Maksymilian Mruszczak <u@1ux.org>
2019-10-18 11:22:53 +0200

    Init

---
+++ segment.c
@@ -0,0 +1,178 @@
+/* Copyright (c) Maksymilian Mruszczak */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <pwd.h>
+
+enum {
+	DELIM,
+	SUBDEL,
+	PROMPT
+};
+
+static const char glyphs[][4] = {
+	{0xee, 0x82, 0xb0, 0x00},
+	{0xee, 0x82, 0xb1, 0x00},
+	"%"
+};
+
+static const char delim[] = {0xee, 0x82, 0xb0, 0x00};
+static const char sdelim[] = {0xee, 0x82, 0xb1, 0x00};
+static const char prp[] = "%";
+
+
+typedef struct Segment Segment;
+struct Segment {
+	Segment *next;
+	unsigned int bg, fg;
+	int bold;
+	char *content;
+};
+
+typedef struct Path Path;
+struct Path {
+	Path *next;
+	char *dir;
+};
+
+
+Segment *
+new(char *content, unsigned char bg, unsigned int fg)
+{
+	Segment *s = (Segment *)malloc(sizeof(Segment));
+	s->bg = bg;
+	s->fg = fg;
+	s->bold = 0;
+	s->next = NULL;
+	s->content = (char *)malloc(strlen(content)+sizeof(char));
+	strcpy(s->content, content);
+	return s;
+}
+
+Segment *
+tail(Segment *s)
+{
+	while (s->next != NULL)
+		s = s->next;
+	return s;
+}
+
+void
+prune(Segment *s)
+{
+	Segment *n;
+	while (s != NULL) {
+		n = s->next;
+		free(s->content);
+		free(s);
+		s = n;
+	}
+}
+
+void
+print(Segment *s)
+{
+	if (s->bold)
+		printf("\x1b[1m");
+	printf("\x1b[48;5;%d;38;5;%dm %s ", s->bg, s->fg, s->content);
+	if (s->next != NULL)
+		printf("\x1b[0;48;5;%d;38;5;%dm%s", s->next->bg, s->bg, delim);
+	else
+		printf("\x1b[0;38;5;%dm%s\x1b[0m ", s->bg, delim);
+}
+
+Path *
+path(char *str)
+{
+	char buf[512];
+	Path *p, *c;
+	int i, offs = 1;
+	p = (Path *)malloc(sizeof(Path));
+	p->dir = (char *)malloc(sizeof(char)*2);
+	p->dir[0] = '/';
+	p->dir[1] = '\0';
+	c = p;
+	for (i=0; str[offs+i] != '\0'; ++i) {
+		if (str[offs+i] == '/') {
+			buf[i] = '\0';
+			offs += i+1;
+			i = -1;
+			c->next = (Path *)malloc(sizeof(Path));
+			c = c->next;
+			c->dir = (char *)malloc(strlen(buf)+sizeof(char));
+			strcpy(c->dir, buf);
+			c->next = NULL;
+		} else
+			buf[i] = str[offs+i];
+	}
+	/* tmp; in case of lack of trailing slash at the end */
+	if (i > 0) {
+		buf[i] = '\0';
+		c->next = (Path *)malloc(sizeof(Path));
+		c = c->next;
+		c->next = NULL;
+		c->dir = (char *)malloc(strlen(buf)+sizeof(char));
+		strcpy(c->dir, buf);
+	}
+	return p;
+}
+
+Segment *
+user(void)
+{
+	struct passwd *pw;
+	pw = getpwuid(getuid());
+	Segment *s = new(pw->pw_name, 39, 26);
+	return s;
+}
+
+Segment *
+pwd(void)
+{
+	Path *p, *c;
+	char buf[512], del[6];
+	sprintf(del, " %s ", subdelim);
+	p = path(getenv("PWD"));
+	strcpy(buf, p->dir);
+	for (c=p->next; c != NULL; c=c->next) {
+		strcat(buf, del);
+		strcat(buf, c->dir);
+	}
+	while (p != NULL) {
+		c = p;
+		free(p->dir);
+		free(p);
+		p = c->next;
+	}
+	return new(buf, 238, 248);
+}
+
+Segment *
+prompt(int status)
+{
+	int col = status ? 160 : 236;
+	return new(prp, col, 7);
+}
+
+int
+main(void)
+{
+	Segment *cur, *one = user();
+	one->next = pwd();
+	one->next->next = new("%", 236, 7);
+	one->next->next->bold = 1;
+	tail(one)->next = pwd();
+	tail(one)->next = new("attention", 226, 233);
+	tail(one)->next = prompt(1);
+	for (cur = one; cur != NULL; cur = cur->next)
+		print(cur);
+	Path *p = path(getenv("HOME"));
+	/*
+	for (; p != NULL; p=p->next)
+		printf("%s\n", p->dir);
+		*/
+	return 0;
+}
